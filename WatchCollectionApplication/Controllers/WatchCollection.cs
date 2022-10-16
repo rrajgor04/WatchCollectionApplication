768b153d3fa1818a236cae3409e8b01656eb49cd
Riddhi Rajgor <W0793048@myscc.ca>
2022-10-16 00:51:25 -0400

    made changes in WatchCollection.cs

---
+++ WatchCollectionApplication/Controllers/WatchCollection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace WatchCollectionApplication.Controllers
+{
+    public class HelloWorldController: Controller
+    {
+       public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        public string Welcome()
+        {
+            return "this is the Welcome action method...";
+        }
+    }
+}
--- WatchCollectionApplication/README.txt
+++ WatchCollectionApplication/README.txt
@@ -1,3 +1,6 @@
 ﻿***16/10/2022***
 12:35am- made README file 
 12:36am - run the application and successfully worked.
+12:37am - started part2
+12:38am added a controller.
+12:50am - made changes in WatchCollection.cs and run the application and it went successfull.
